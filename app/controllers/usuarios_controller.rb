a0b954a372da44fea94b0059208c082b9dc0e574
Lucas Henrique <lucas50xd@hotmail.com>
2015-12-03 01:15:55 -0200

    Implementação gráfica e funcional do sign up

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -69,6 +69,6 @@ class UsuariosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def usuario_params
-      params.require(:usuario).permit(:usuario, :senha)
+      params.require(:usuario).permit(:usuario, :password)
     end
 end
